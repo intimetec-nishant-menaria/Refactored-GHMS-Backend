8b8f390e153457b16eaf7c60cebe9a89bf18274c
Khushi vijay <khushi.vijay@intimetec.com>
2026-02-26 11:30:49 +0530

    Adding the required Enums for the models

---
+++ Enums/BookingStatusEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace guest_house_management_backend.Enums
+{
+    public enum BookingStatusEnum
+    {
+        Pending = 0,
+        Confirmed = 1,
+        Cancelled = 2,
+        Completed = 3
+    }
+}
---
+++ Enums/RoomStatusEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace guest_house_management_backend.Enums
+{
+    public enum RoomStatusEnum
+    {
+        Available = 1,
+        Occupied = 2,
+    }
+}
