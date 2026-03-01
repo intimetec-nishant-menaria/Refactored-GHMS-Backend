9e5343d9ea7d4809095f90a7987ec30feff8dcfe
Khushi vijay <khushi.vijay@intimetec.com>
2026-03-01 16:42:46 +0530

    Adding the Controller of the Booking

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,62 @@
+﻿using guest_house_management_backend.DTOs;
+using guest_house_management_backend.Services.Bookings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace guest_house_management_backend.Controllers
+{
+    [Route("api/bookings")]
+    [ApiController]
+    public class BookingController : Controller
+    {
+        private readonly IBookingService _service;
+        public BookingController(IBookingService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            return Ok(await _service.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking(CreateBookingDto createRequest)
+        {
+            var booking = await _service.CreateAsync(createRequest);
+            return Ok(new
+            {
+                message = "Booking Created successfully.",
+                data = booking
+            });
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateBookingDto updateRequest)
+        {
+            await _service.UpdateAsync(id, updateRequest);
+
+            return Ok(new
+            {
+                message = "Booking updated successfully."
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.DeleteAsync(id);
+
+            return Ok(new
+            {
+                message = "Booking deleted successfully."
+            });
+        }
+    }
+}
