aa5be1c24b6a03ede5d0117bab8138f701131975
Khushi vijay <khushi.vijay@intimetec.com>
2026-03-01 13:12:22 +0530

    Creating the Booking Model with all the required fields

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -6,25 +6,22 @@ namespace guest_house_management_backend.Models
 {
     public class Booking
     {
+        [Key]
         public int Id { get; set; }
-
         [Required]
         public int RoomId { get; set; }
         public Room Room { get; set; } = null!;
-
         [Required]
         public int GuestId { get; set; }
         public Guest Guest { get; set; } = null!;
-
         [Required]
         public DateTime CheckInDate { get; set; }
-
         [Required]
         public DateTime CheckOutDate { get; set; }
-
         [Required]
-        public BookingStatusEnum Status { get; set; } = BookingStatusEnum.Pending;
-        
+        public BookingStatusEnum Status { get; set; }
+        public decimal price { get; set; }
+        public string? SpecialRequests { get; set; }
         [Required]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; }
