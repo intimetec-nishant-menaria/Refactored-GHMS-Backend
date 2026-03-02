5a704910ec9ba429a60fa1f4c14235e34d1bae6a
Khushi vijay <khushi.vijay@intimetec.com>
2026-03-02 09:39:47 +0530

    Add the interface of Available room repository and its implementation

---
+++ Repositories/AvailableRoomRepo/AvailRoomRepostiory.cs
@@ -0,0 +1,30 @@
+﻿using guest_house_management_backend.Data;
+using guest_house_management_backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace guest_house_management_backend.Repositories.AvailableRoomRepo
+{
+    public class AvailRoomRepostiory : IAvailRoomRepository
+    {
+        private readonly DBContext _context;
+        public AvailRoomRepostiory(DBContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Room>> GetAvailableRoomAsync(DateTime checkIn, DateTime checkOut)
+        {
+            var bookedRoomIds = await _context.Bookings
+                 .Where(b =>
+                     b.Status != Enums.BookingStatusEnum.Cancelled &&
+                     b.Status != Enums.BookingStatusEnum.Completed &&
+                     b.CheckInDate < checkOut &&
+                     b.CheckOutDate > checkIn)
+                 .Select(b => b.RoomId)
+                 .ToListAsync();
+            return await _context.Room
+                .Where(r => !bookedRoomIds.Contains(r.Id))
+                .AsNoTracking()
+                .ToListAsync();
+        }
+    }
+}
